dd6faec89983e4a839073d45c686064de5cc0c3e
MSI\dhava <dhavamba@gmail.com>
2018-01-27 14:48:36 +0100

    added UI

---
+++ Jam 2018/Assets/_Game_/Scripts/Bar.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class Bar : MonoBehaviour
+{
+    protected RectTransform rect;
+    private float maxSixe;
+
+    private float time;
+    private float oldTime;
+
+    protected virtual void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        maxSixe = rect.sizeDelta.x;
+        oldTime = time;
+    }
+
+    private void Update()
+    {
+        if (time != 0)
+        {
+            if (oldTime > 0)
+            {
+                SetBar(oldTime / time);
+                oldTime -= Time.deltaTime;
+            }
+            else
+            {
+                FinishTime();
+            }
+        }
+    }
+
+    public void SetTime(float t)
+    {
+        time = t;
+        oldTime = time;
+    }
+
+    private void FinishTime()
+    {
+        SetBar(0);
+        time = 0;
+    }
+
+    protected void SetBar(float actualValue)
+    {
+        rect.sizeDelta = new Vector2(maxSixe * actualValue, rect.sizeDelta.y);
+    }
+}
