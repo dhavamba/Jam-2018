0c91d27a666e7404ec1f34a698be131cd0a64007
MSI\dhava <dhavamba@gmail.com>
2018-01-27 11:46:35 +0100

    add interface input

---
+++ Jam 2018/Assets/_Game_/Scripts/InterfaceInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Rewired;
+using System;
+
+//The class is an interface between Rewired and the Game
+public class InterfaceInput : Singleton<InterfaceInput>
+{
+    private Player player;
+
+    public Player Player
+    {
+        get
+        {
+            return player;
+        }
+    }
+
+    // Use this for initialization
+    private void Awake ()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
+
+}
---
+++ Jam 2018/Assets/_Game_/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float unit;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GetLeft())
+        {
+            transform.Translate(Vector2.left * unit);
+        }
+        if (GetRight())
+        {
+            transform.Translate(Vector2.right * unit);
+        }
+    }
+
+
+    private bool GetLeft()
+    {
+        return InterfaceInput.Instance().Player.GetButtonDown("left");
+    }
+
+    private bool GetRight()
+    {
+        return InterfaceInput.Instance().Player.GetButtonDown("right");
+    }
+}
