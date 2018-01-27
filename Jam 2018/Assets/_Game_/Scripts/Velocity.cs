8908c770b72d9193c5a10fa2a7af365685149dbc
MSI\dhava <dhavamba@gmail.com>
2018-01-27 19:10:16 +0100

    added prefabs

---
+++ Jam 2018/Assets/_Game_/Scripts/Velocity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.down * speed);
+        Invoke("Destroy", 10f);
+	}
+
+    void Destroy()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
