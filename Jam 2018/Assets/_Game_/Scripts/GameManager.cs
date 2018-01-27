1eb7f363250119fa95d0dab7a5cecf257595a91f
pc-federico\Federico <f.distefano87@hotmail.it>
2018-01-27 11:46:59 +0100

    committato script

---
+++ Jam 2018/Assets/_Game_/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float timeEvent; //il tempo di durata di un evento
+    public int level; //il livello attuale
+    public float startDelay;
+    private float timerStart;
+    //public float timeAnimation; //il tempo di durata dell'animazione
+    //public float 
+
+	void Start ()
+    {
+		timerStart = Time.time;
+    }
+	
+	void Update ()
+    {
+		//Inizia il livello
+
+	}
+}
